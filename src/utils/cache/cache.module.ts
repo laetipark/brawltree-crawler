fe73343f6aba20241e85cc6a802cebc40aed5f0e
Donghun Park <creator98@naver.com>
2024-03-11 22:09:38 +0900

    Refactor: setting environment variables for the cache module

--- src/utils/cache/cache.module.ts
+++ src/utils/cache/cache.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-ioredis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    CacheModule.register({
-      isGlobal: true,
-      store: redisStore,
-      host: 'localhost',
-      port: 6379,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        isGlobal: true,
+        store: redisStore,
+        host: config.get<string>('redis.host'),
+        port: config.get<number>('redis.port'),
+      }),
     }),
   ],
+  exports: [CacheModule],
 })
 export class UtilCacheModule {}
